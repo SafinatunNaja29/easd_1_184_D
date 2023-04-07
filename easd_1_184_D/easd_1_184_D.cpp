c57e92ad247b11b6c9aed9de623df3c52813ab43
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-04-07 09:57:41 +0700

    menambahkan kondisi i

--- easd_1_184_D/easd_1_184_D.cpp
+++ easd_1_184_D/easd_1_184_D.cpp
@@ -19,10 +19,17 @@ void input() {		//Procedure input
 		cout << "Masukkan panjang element array : ";
 		cin >> n;
 
-		if (n <= 104)	
+		if (n <= 104)
 			break;
 		else
 			cout << "\nMaksimum panjang array adalah 104" << endl;
-
+	}
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> saff[i];
 	}
 }
