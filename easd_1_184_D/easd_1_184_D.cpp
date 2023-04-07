db7d17ec6667254bba1560810121dd3901f81d01
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-04-07 09:47:24 +0700

    deklarasi variable array

---
+++ easd_1_184_D/easd_1_184_D.cpp
@@ -0,0 +1,14 @@
+// 1. karena algoritma digunakan untuk merancang suatu program 
+// yang dapat memecahkan masalah yang kompleks dan juga memberikan cara yang sistematis
+// 2. Struktur data linier dan struktur data non-linier
+// 3. efisiensi suatu program dapat dipengaruhi oleh penggunaan variabel, penggunaan struktur data, 
+// memory.
+// 4. Insertion Sort, karena ini dapat membandingkan dan mengurutkan elemen-elemen ke posisi
+// yang benar/urut
+// 5. Quadratic(Insertion Sort, Bubble Sort, Selection Sort) Loglinear (Quick Sort dan Merge Sort)
+
+#include <iostream>
+using namespace std;
+
+int saff[104]; // deklarasi variable array
+int n;
